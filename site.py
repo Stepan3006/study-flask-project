0abfe0f0b1e33901435e6d816e36a1cab626ea93
Stepan3006 <stepan@onepk.ru>
2022-11-07 17:12:43 +0300

    Add htmk templates

--- site.py
+++ site.py
@@ -1,15 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+menu = ['Установка', 'Первое приложение', 'Обратная связь']
 
 @app.route("/index")
 @app.route("/")
 def index():
-    return 'index'
+    return render_template('index.html', title = 'About Flack', menu=menu)
 
 @app.route("/about")
 def about():
     return '<h1>О Сайте</h1>'
+
+@app.route("/author")
+def author():
+    return render_template('author.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
